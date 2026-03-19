5d259b3c5e61f71bffe0ad8e28f43c214dd86b1b
amartopalovic <amartopalovic27@gmail.com>
2026-03-19 20:43:10 +0100

    Add the starting files

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { ChatInput } from "./components/ChatInput";
+import ChatMessages from "./components/ChatMessages";
+import "./App.css";
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello! How can I help you?",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "Can you get me todays date",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "Today is March 1",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+  //    First           Second
+  // const [chatMessages, setChatMessages] = array;
+
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
+
+export default App;
